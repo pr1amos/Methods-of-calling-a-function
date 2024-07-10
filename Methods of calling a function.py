dfddf7ddffd323eb435f97deae6b6fe6a31733db
pr1amos <157016419+pr1amos@users.noreply.github.com>
2024-07-10 10:30:05 +0300

    Update Methods of calling a function.py

--- Methods of calling a function.py
+++ Methods of calling a function.py
@@ -17,7 +17,7 @@ def send_email(message, recipient, sender="university.help@gmail.com"):
   else:
     print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
 
-# Пример выполнения кода
+
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
